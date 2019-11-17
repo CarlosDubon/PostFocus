871270221c2730508b7f2adba9fcb43008ff264c
Carlos Dubon <carlosminerodubon@gmail.com>
2019-11-16 22:38:58 -0600

    first render

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,11 +55,69 @@ const getPosts=(users)=>{
     
 }
 
-async function main(){
+
+const getGenders=(user)=>{
+    return new Promise(resolve=>{
+        fetch(`https://api.genderize.io?name=${user.name.split(" ")[0]}`,configGet)
+        .then(res=>{
+            if(res.ok){
+                res.json()
+                .then(data=>{
+                    const {gender} = data
+                    resolve(gender)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })  
+    })      
+    
+}
+
+async function render(){
     var users = []
+
     users = await getUsers()
     users = await getPosts(users)
+    var $container = document.getElementById("main-container")
+   
+    for(const u of users){
+        let $div = document.createElement("div")
+        let $profileContainer = document.createElement("div")
+        let $postContainer = document.createElement("div")
+        let $nameText = document.createTextNode(u.name)
+        let $name = document.createElement("h5")
+        let $avatar = document.createElement("img")
+
+        let gender = await getGenders(u)
+        gender==="male"?gender = "/assets/img/jacques.svg":gender = "/assets/img/josephine.svg"
+        $avatar.src = gender
+    
+        u.posts.forEach(post=>{
+            let $post = document.createElement("p")
+            let $textPost  = document.createTextNode(post.title)
+            
+            $post.append($textPost)
+
+            $postContainer.append($post)
+        })
+
+
+        //Append section
+        $name.append($nameText)
+        $profileContainer.append($avatar)
+        $profileContainer.append($name)
+        $div.append($profileContainer)
+        $div.append($postContainer)
+        $container.append($div)
+    }
+        
+
 }
 
 
-window.addEventListener('load',main())
+window.addEventListener('load',render())
